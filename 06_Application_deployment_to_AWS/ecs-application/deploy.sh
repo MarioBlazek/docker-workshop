deda0a61f5ab5952627a66b3d08f65ea09c0170c
Mario Blazek <blazek.mario@gmail.com>
2022-11-23 00:18:14 +0100

    Part 6 deploy script

--- 06_Application_deployment_to_AWS/ecs-application/deploy.sh
+++ 06_Application_deployment_to_AWS/ecs-application/deploy.sh
@@ -2,17 +2,20 @@
 
 set -e
 
-AWS_ACCOUNT_ID=1111111111
+read -rp 'Enter AWS Account ID: ' AWS_ACCOUNT_ID
+
+export AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID
 
 docker context use default
 docker-compose -f docker-compose-dev.yml build
 
-docker tag ecs-application_backend:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/docker-workshop-backend
-docker tag ecs-application_frontend:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/docker-workshop-frontend
+docker tag ecs-application_backend:latest "$AWS_ACCOUNT_ID".dkr.ecr.us-east-1.amazonaws.com/docker-workshop-backend
+docker tag ecs-application_frontend:latest "$AWS_ACCOUNT_ID".dkr.ecr.us-east-1.amazonaws.com/docker-workshop-frontend
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID".dkr.ecr.us-east-1.amazonaws.com
 
-docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/docker-workshop-backend
+docker push "$AWS_ACCOUNT_ID".dkr.ecr.us-east-1.amazonaws.com/docker-workshop-backend
+# shellcheck disable=SC2086
 docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/docker-workshop-frontend
 
 
--- 06_Application_deployment_to_AWS/ecs-application/down.sh
+++ 06_Application_deployment_to_AWS/ecs-application/down.sh
@@ -2,5 +2,9 @@
 
 set -e
 
+read -rp 'Enter AWS Account ID: ' AWS_ACCOUNT_ID
+
+export AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID
+
 docker context use docker-workshop-ecs
 docker compose -f docker-compose-prod.yml down
